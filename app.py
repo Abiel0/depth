16945a046265bce72b5aad53af54464751ded03e
myjesusmysaviour <usagecomputer1@gmail.com>
2024-10-12 15:00:19 +0100

    first commit

--- app.py
+++ app.py
@@ -64,4 +64,4 @@ def generate_depth_map():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
